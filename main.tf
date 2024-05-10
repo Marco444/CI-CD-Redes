08e8f54fc5766031967d8b659fb95554147700b8
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-10 17:21:27 -0300

    added load balancer to EC2 and removed public IP

--- main.tf
+++ main.tf
@@ -35,9 +35,9 @@ resource "aws_internet_gateway" "igw" {
 
 # Create a subnet within the VPC
 resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = false  # Changed to false to not assign public IPs
 
   tags = {
     Name = "ExampleSubnet"
@@ -64,10 +64,31 @@ resource "aws_route_table_association" "rta" {
   route_table_id = aws_route_table.rt.id
 }
 
-# Security Group to allow traffic on port 80
+# Security Group to allow traffic on port 80 from ELB
 resource "aws_security_group" "sg" {
   name        = "sg"
   description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]  # Allow only from load balancer
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security group for the load balancer
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_sg"
+  description = "Load balancer security group"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
@@ -85,28 +106,70 @@ resource "aws_security_group" "sg" {
   }
 }
 
+# Load Balancer
+resource "aws_lb" "elb" {
+  name               = "my-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.subnet.id]
+}
+
+# Target Group
+resource "aws_lb_target_group" "tg" {
+  name     = "my-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Listener
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.elb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
 
 # EC2 Instance
 resource "aws_instance" "ec2" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.subnet.id
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  ami                     = data.aws_ami.amazon_linux.id
+  instance_type           = var.instance_type
+  subnet_id               = aws_subnet.subnet.id
+  vpc_security_group_ids  = [aws_security_group.sg.id]
 
   tags = {
     Name = "MyEC2Instance"
   }
 
   user_data = <<-EOF
-              #!/bin/bash
-              sudo yum -y update
-              sudo yum -y install httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              EOF
+                #!/bin/bash
+                sudo yum -y update
+                sudo yum -y install httpd
+                sudo systemctl start httpd
+                sudo systemctl enable httpd
+                EOF
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
+# Attach EC2 to the target group
+resource "aws_lb_target_group_attachment" "tga" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.ec2.id
+  port             = 80
+}
