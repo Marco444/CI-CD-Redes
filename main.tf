b4f269406c9b7de8c38329be8ee2d82374bbc08c
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-10 19:01:58 -0300

    added actions

--- main.tf
+++ main.tf
@@ -137,20 +137,20 @@ resource "aws_lb" "elb" {
 }
 
 # Target Group
-resource "aws_lb_target_group" "tg" {
-  name     = "my-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
+# resource "aws_lb_target_group" "tg" {
+#   name     = "my-tg"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = aws_vpc.vpc.id
+#
+#   health_check {
+#     path                = "/"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#   }
+# }
 
 # Listener
 resource "aws_lb_listener" "listener" {
