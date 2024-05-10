20c7aa3cea856f4559df22de6608143907c616c0
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-10 16:11:38 -0300

    barebones ec2 demo

--- main.tf
+++ main.tf
@@ -1,35 +1,9 @@
-provider "aws" {
-  region = var.aws_region
-}
+resource "aws_instance" "ec2-demo" {
+  ami           = "ami-04ff98ccbfa41c9ad"
+  instance_type = "t2.micro"
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-  key_name      = var.key_name
 
   tags = {
-    Name = var.name
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum -y update
-              sudo yum -y install httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              EOF
-
-  lifecycle {
-    create_before_destroy = true
+    Name = "HelloWorld"
   }
 }
