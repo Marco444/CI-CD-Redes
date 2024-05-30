1f7d74feadf7192e44eb68550b336cb46c9b9e14
Marco4444 <mscilipoti@itba.edu.ar>
2024-05-30 18:35:09 -0300

    removed lock for testing

--- LendARead2-AWS/terraform/prod/main.tf
+++ LendARead2-AWS/terraform/prod/main.tf
@@ -5,4 +5,7 @@ module "main" {
   ecs_task_cpu_architecture = "X86_64"
   aws_region = "us-east-1"
   vpc_cidr="10.3.0.0/16"
+  multi_az_rds=var.multi_az_rds
+  role=var.role
+  rds_password=var.rds_password
 }
--- LendARead2-AWS/terraform/qa/main.tf
+++ LendARead2-AWS/terraform/qa/main.tf
@@ -1,7 +1,10 @@
 
 module "main" {
-  source            = "../modules/main"
+  source                    = "../modules/main"
   ecs_task_cpu_architecture = "X86_64"
-  aws_region = "us-east-1"
-  vpc_cidr="10.2.0.0/16"
+  aws_region                = "us-east-1"
+  vpc_cidr                  = "10.2.0.0/16"
+  multi_az_rds              = var.multi_az_rds
+  role                      = var.role
+  rds_password              = var.rds_password
 }
